bf00ecc9b3c3197ed2cb71e1ad6c07461dfee725
takuma.matsuzaki <takuma.takuma0928@gmail.com>
2022-04-06 10:44:31 +0000

    Change the router

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesController@index');
+
+/*
+//メッセージの個別最小ページ表示
+Route::get('messages/{id}', 'MessagesController@show');
+//メッセージの個別登録処理
+Route::post('messages', 'MessagesController@store');
+//メッセージの更新処理
+Route::put('messages/{id}', 'MessagesController@update');
+//メッセージの削除
+Route::delete('messages/{id}', 'MessagesController@destroy');
+
+//一覧表示のページ
+Route::get('messages', 'MessagesController@index')->name('messages.index');
+//新規作成フォームのページ
+Route::get('messages/create', 'MessagesController@create')->name('messages.create');
+//更新用のフォームページ
+Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit');
+*/
+
+Route::resource('messages', 'MessagesController');
